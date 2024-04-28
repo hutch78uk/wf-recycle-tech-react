68b8b356e0c6d13c7f67e3d91f94e5834309b419
hutch78uk <hutch78uk@yahoo.com>
2024-04-28 23:39:25 +0100

    Responsive nav fine tuning

--- src/components/shared/Navigation.tsx
+++ src/components/shared/Navigation.tsx
@@ -41,13 +41,13 @@ function Navigation() {
                 <span className={open ? 'closeBtn':'navburger'} onClick={()=>setOpen(!open)}></span>
             </label>
             <div className={`absolute md:relative w-full overflow-hidden z-[1] 
-                md:h-auto transition-height duration-300 ease-in ${open ? 'h-[337px]':'h-0'}`}>
+                md:h-auto transition-height duration-500 ease-in ${open ? 'h-[337px]':'h-0'}`}>
                 <nav className={`w-full mt-px md:mt-0 bg-grayLight md:bg-white 
                     shadow-[0_5px_5px_rgba(0,0,0,0.1)] md:shadow-none absolute md:relative 
-                    transition-all duration-300 ease-in md:top-0 ${open ? 'top-0':'top-[-380px]'}`}>
+                    transition-all duration-500 ease-in md:top-0 ${open ? 'top-0':'top-[-380px]'}`}>
                     <ul className="mt-2.5 md:mt-0 md:flex md:justify-between text-center">
                         {navItems.map(navItem => <li key={navItem.name} className=
-                        "mb-2.5 md:mb-0 pb-2.5 border-b border-white last-of-type:border-none text-gray666 hover:text-wfGreenLight">
+                        "mb-2.5 last-of-type:mb-0 md:mb-0 pb-2.5 border-b border-white last-of-type:border-none text-gray666 hover:text-wfGreenLight">
                             <NavLink onClick={()=>setOpen(!open)} className={'relative opacity-100'} to={navItem.path}>{navItem.name}</NavLink></li>)}
                     </ul>
                 </nav>
