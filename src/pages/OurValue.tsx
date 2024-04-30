4223c42d11cece1f01c3164b81f872b6cf421f84
hutch78uk <hutch78uk@yahoo.com>
2024-04-30 17:28:26 +0100

    Our Value responsive styling

--- src/pages/OurValue.tsx
+++ src/pages/OurValue.tsx
@@ -10,7 +10,7 @@ const OurValue = () => {
         <>
             <section className="section">
                 <div className="mx-auto w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1200px]">
-                    <h1 className="text-center text-gray777 mt-2.5 mb-[30px] text-[2.125rem]">Experienced Engineering Team</h1>
+                    <h1 className="h2">Experienced Engineering Team</h1>
                     <p className="text-gray666">One of WF Recycle-Tech&apos;s key sources of added value is through its reliable engineering partners 
                         Farrel Pomini, one of the world&apos;s leading providers of continuous compounding systems with over 100 years&apos; 
                         experience in the polymer industry. Farrel Pomini designs, manufactures and supports world-class continuous 
@@ -25,30 +25,30 @@ const OurValue = () => {
                                 Recycle-Tech model of keeping its system design simple, using tried and tested technology and with a wealth of 
                                 engineering experience at its side, this risk is addressed.</p>
                         </div>
-                        <div>
-                            <img src={farrelPomoniOffices} alt="Farrel Pomini head offices" />
+                        <div className="mt-5 md:mt-0">
+                            <img src={farrelPomoniOffices} className="mx-auto" alt="Farrel Pomini head offices" />
                         </div> 
                     </div>
                 </div>
             </section>
             <section className="section bg-grayLight py-[30px]">
                 <div className="mx-auto w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1200px]">
-                    <h1 className="text-center text-gray777 mt-2.5 mb-[30px] text-[2.125rem]">Experienced Engineering Team</h1>
+                    <h1 className="h2">Experienced Engineering Team</h1>
                     <div className="md:grid grid-cols-2 gap-4 mt-5">
                         <div>
                             <p>The WF Recycle-Tech system processes 1.5 tonnes of rubber crumb from End of Life Tyres (ELTs) per hour and has been designed with this specific throughput in mind so that companies can benefit from numerous small scale systems
                             placed strategically in various locations. The advantage being that the customer can benefit from placing facilities closer to various origins for the collection of ELTs as well as potentially housing a system close to third
                             party clients for the sale of the offtakes whether that be for the oil or rCB. This differs from single large scale production facilities which don&apos;t have such flexibility and enables customers to minimise transportation costs.</p>
                         </div>
-                        <div>
-                            <img src={pyrolysisPlant} alt="A pyrolysis plant" />
+                        <div className="mt-5 md:mt-0">
+                            <img src={pyrolysisPlant} className="mx-auto" alt="A pyrolysis plant" />
                         </div> 
                     </div>
                 </div>
             </section>
             <section className="section">
                 <div className="mx-auto w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1200px]">
-                    <h1 className="text-center text-gray777 mt-2.5 mb-[30px] text-[2.125rem]">Unique Patented Process</h1>
+                    <h1 className="h2">Unique Patented Process</h1>
                     <div className="md:grid grid-cols-2 gap-4 mt-5">
                         <div>
                             <p>Having had four British patents granted, WF Recycle-Tech are in a strong position with regard to the 
@@ -57,15 +57,15 @@ const OurValue = () => {
                                 led to accredited companies such as Artis acknowledging the unique qualities of the WF Recycle-Tech rCB including a high level particle dispersion
                                 prior to any post processing applications.</p>
                         </div>
-                        <div>
-                            <img src={patented} alt="Patented image" />
+                        <div className="mt-5 md:mt-0">
+                            <img src={patented} className="mx-auto" alt="Patented image" />
                         </div> 
                     </div>
                 </div>
             </section>
             <section className="section bg-grayLight py-[30px]">
                 <div className="mx-auto w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-[1200px]">
-                    <h1 className="text-center text-gray777 mt-2.5 mb-[30px] text-[2.125rem]">Continuous & Efficient Operation</h1>
+                    <h1 className="h2">Continuous & Efficient Operation</h1>
                     <div className="md:grid grid-cols-2 gap-4 mt-5">
                         <div>
                             <p>Unlike many other companies in the industry who proceed with batch operations, the WF Recycle-Tech solution 
@@ -73,8 +73,8 @@ const OurValue = () => {
                                 reduced manual labour. The system also takes up a very small footprint in relation to most pyrolysis units 
                                 with all the core components installed in less than a 20m x 40m area.</p>
                         </div>
-                        <div>
-                            <img src={continuousProcess} alt="Tyres on a conveyor belt" />
+                        <div className="mt-5 md:mt-0">
+                            <img src={continuousProcess} className="mx-auto" alt="Tyres on a conveyor belt" />
                         </div> 
                     </div>
                 </div>
